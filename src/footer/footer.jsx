b82fe336cb89167a6bfd5b10976fc50520ef6b8a
aishikanandi <aishikanandi@gmail.com>
2023-05-12 12:34:31 +0530

    commit again

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -4,7 +4,7 @@ export default function footer() {
   return (
     <div className='credits'>
       <p>Developed by Aishika Nandi &copy; 2023 </p>
-      <p>Learn React</p>
+      <p>learn react</p>
     </div>
   )
 }
